5c1241db0c1428786ae86f06837157f3c48dfc9a
Andressa Silva <andressaszx@gmail.com>
2022-08-10 14:50:32 -0300

    Install bootstrap

--- src/app/topo/topo.component.ts
+++ src/app/topo/topo.component.ts
@@ -3,5 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-topo',
   templateUrl: './topo.component.html',
+  styleUrls: ['./topo.component.scss']
 })
 export class TopoComponent {}
